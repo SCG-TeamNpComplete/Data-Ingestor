647dc5192cf1d45f00938a55114b2a8e23c0ff2e
Kedar <kedar.gn20@gmail.com>
2016-10-18 18:54:45 -0400

    added log to shell script

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,5 +3,5 @@ cd '/home/ec2-user/dataingestor-microservice/SG_MICROSERVICE_DATAINGESTOR'
 sudo mvn -e clean install >> /var/log/tomcat.log
 sudo cp target/*.war /usr/local/tomcat7/apache-tomcat-7.0.72/webapps/ >> /var/log/tomcat.log
 sudo cd  /usr/local/tomcat7/apache-tomcat-7.0.72
-
+pwd >> /var/log/tomcat.log
 sudo sh ./bin/startup.sh >> /var/log/tomcat.log 2>&1 &
