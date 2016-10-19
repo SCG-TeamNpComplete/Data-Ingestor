2fdd75b5e6779124974ea1ae4770c4d27003d510
Kedar <kedar.gn20@gmail.com>
2016-10-18 23:09:09 -0400

    removed the useless path

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,5 @@
 echo 'Installing SG_MICROSERVICE_DATAINGESTOR' >> /var/log/tomcat.log
-cd '/home/ec2-user/dataingestor-microservice/SG_MICROSERVICE_DATAINGESTOR'
+cd '/home/ec2-user/SG_MICROSERVICE_DATAINGESTOR'
 sudo mvn -e clean install >> /var/log/tomcat.log
 sudo cp target/*.war /usr/local/tomcat7/apache-tomcat-7.0.72/webapps/ >> /var/log/tomcat.log
 sudo sh /usr/local/tomcat7/apache-tomcat-7.0.72/bin/startup.sh >> /var/log/tomcat.log 2>&1 &
