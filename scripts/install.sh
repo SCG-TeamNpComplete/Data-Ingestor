a6af5b9c91574587be610dc0c84934c3f8777176
Kedar <kedar.gn20@gmail.com>
2016-10-21 02:38:33 -0400

    fix port no in docker run

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,4 +2,4 @@ echo 'starting installation process'
 cd '/home/ec2-user/docker'
 sudo docker login -e="kedar.gn20@gmail.com" -u="kedargn" -p="npcomplete"   #TODO : hide password
 sudo docker pull kedargn/dataingestor
-sudo docker run -d -p 64000:64000 --name dataingestor $(sudo docker images | grep kedargn/dataingestor | awk '{print $3}') >> ./log.txt
+sudo docker run -d -p 8080:8080 --name dataingestor $(sudo docker images | grep kedargn/dataingestor | awk '{print $3}') >> ./log.txt
