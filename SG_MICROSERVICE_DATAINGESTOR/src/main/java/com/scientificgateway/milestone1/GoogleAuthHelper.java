1f3f3a7a1cea861bda7d85f9181b66adeab84b21
Abhinandan Sampathkumar <abhinandan.cs001@gmail.com>
2016-10-21 19:52:15 -0400

    New ip for new instance

--- SG_MICROSERVICE_DATAINGESTOR/src/main/java/com/scientificgateway/milestone1/GoogleAuthHelper.java
+++ SG_MICROSERVICE_DATAINGESTOR/src/main/java/com/scientificgateway/milestone1/GoogleAuthHelper.java
@@ -32,7 +32,7 @@ public class GoogleAuthHelper {
 	/**
 	 * Callback URI that google will redirect to after successful authentication
 	 */
-	private static final String CALLBACK_URI = "http://ec2-54-69-92-137.us-west-2.compute.amazonaws.com:8085/SG_MICROSERVICE_DATAINGESTOR/webapi/home";
+	private static final String CALLBACK_URI = "http://ec2-35-160-231-198.us-west-2.compute.amazonaws.com:8085/SG_MICROSERVICE_DATAINGESTOR/webapi/home";
 
 	// start google authentication constants
 	private static final Iterable<String> SCOPE = Arrays
