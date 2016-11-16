c063d13f12e2edbcdac68d4e5ce9ca1f9882c68d
Kedar <kedar.gn20@gmail.com>
2016-11-16 16:13:09 -0500

    replicate instances #1

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,23 @@
-echo 'starting installation process'
+#!/bin/bash
+
 cd '/home/ec2-user/docker'
 sudo docker login -e="kedar.gn20@gmail.com" -u="kedargn" -p="npcomplete"   #TODO : hide password
 sudo docker pull kedargn/dataingestor
-sudo docker run -d -p 8085:8080 --name dataingestor $(sudo docker images | grep kedargn/dataingestor | awk '{print $3}') >> ./log.txt
+
+no_of_instances=3     #change this to set the number of instances
+current_instance=1
+port=8085
+while [ $current_instance -le $no_of_instances ]
+do
+	#echo "$(sudo docker ps -a | grep "forecastdetector$current_instance" | awk '{print $1}')"
+	if [ "$(sudo docker ps -a | grep "dataingestor$current_instance" | awk '{print $1}')" != "" ]; then
+		sudo docker ps -a | grep "dataingestor$current_instance" | awk '{print $1}' | xargs --no-run-if-empty sudo docker stop
+		sudo docker ps -a | grep "dataingestor$current_instance" | awk '{print $1}' | xargs --no-run-if-empty sudo docker rm
+	fi
+	#echo "$port:$port"
+	sudo docker run -d -p "$port:$port" --name "dataingestor$current_instance" $(sudo docker images | grep kedargn/dataingestor | awk '{print $3}')
+	echo "dataingestor$current_instance instance"
+	current_instance=$((current_instance+1))
+	port=$((port+1))
+	sleep 1
+done
