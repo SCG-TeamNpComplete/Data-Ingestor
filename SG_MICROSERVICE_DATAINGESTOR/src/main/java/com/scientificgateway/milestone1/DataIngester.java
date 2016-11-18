2e209707d90a145f1acb36b500cffe3309d842e7
asampath <asampath@indiana.edu>
2016-11-18 04:39:07 -0500

    #6 ensemble works, register service during war load

--- SG_MICROSERVICE_DATAINGESTOR/src/main/java/com/scientificgateway/milestone1/DataIngester.java
+++ SG_MICROSERVICE_DATAINGESTOR/src/main/java/com/scientificgateway/milestone1/DataIngester.java
@@ -10,13 +10,58 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.RetryNTimes;
+import org.apache.curator.x.discovery.ServiceDiscoveryBuilder;
+import org.apache.curator.x.discovery.ServiceInstance;
+import org.apache.curator.x.discovery.UriSpec;
 import org.apache.log4j.Logger;
 
 import com.scientificgateway.BeanParams.DataIngesterParamHolder;
 import com.scientificgateway.servicelayer.DataIngesterService;
 
+
+
 @Path("/dataingester")
 public class DataIngester {
+	
+	static{
+
+		System.out.println("Registering Data Ingester Service");
+		String endpointURI = "localhost:8080/SG_MICROSERVICE_DATAINGESTOR/webapi/dataingester/get";
+	    //private final String endpointURI = "http://" + serverName + ":" + serverPort + "/catalog/resources/catalog";
+	    //private final String endpointURI = "http://" + WildFlyUtil.getHostName() + ":" + WildFlyUtil.getHostPort() + "/catalog/resources/catalog";
+	    String serviceName = "dataIngester";
+		
+	    int port = 8080;
+
+		CuratorFramework curatorFramework = CuratorFrameworkFactory.newClient("localhost:2181",
+				new RetryNTimes(5, 1000));
+		curatorFramework.start();
+		try {
+			
+			ServiceInstance serviceInstance = ServiceInstance.builder().uriSpec(new UriSpec(endpointURI)).address("localhost").port(port).name(serviceName).build();
+			ServiceDiscoveryBuilder.builder(Void.class).basePath("load-balancing-example-dataIngester").client(curatorFramework)
+			.thisInstance(serviceInstance).build().start();
+			
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		try {
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	
+	}
+	
+	//DataIngesterInit dataIngester;
 	public static Logger log = Logger.getLogger(DataIngester.class.getName());
 	private DataIngesterService DIservice;
 
--- SG_MICROSERVICE_DATAINGESTOR/src/main/java/com/scientificgateway/milestone1/DataIngesterInit.java
+++ SG_MICROSERVICE_DATAINGESTOR/src/main/java/com/scientificgateway/milestone1/DataIngesterInit.java
@@ -1,5 +1,7 @@
 package com.scientificgateway.milestone1;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
@@ -11,45 +13,77 @@ import org.apache.curator.x.discovery.ServiceInstance;
 import org.apache.curator.x.discovery.UriSpec;
 
 @Path("/DataIngester")
-public class DataIngesterInit {
-	
-	@GET
-	@Path("/init")
-	public String initZookeeper(){
-		
+public class DataIngesterInit extends HttpServlet {
+
+	@Override
+	public void init() throws ServletException {
+
+		System.out.println("Registering Data Ingester Service");
 		String endpointURI = "localhost:8080/SG_MICROSERVICE_DATAINGESTOR/webapi/dataingester/get";
-	    //private final String endpointURI = "http://" + serverName + ":" + serverPort + "/catalog/resources/catalog";
-	    //private final String endpointURI = "http://" + WildFlyUtil.getHostName() + ":" + WildFlyUtil.getHostPort() + "/catalog/resources/catalog";
-	    String serviceName = "dataIngester";
-		
-	    int port = 8080;
+		// private final String endpointURI = "http://" + serverName + ":" +
+		// serverPort + "/catalog/resources/catalog";
+		// private final String endpointURI = "http://" +
+		// WildFlyUtil.getHostName() + ":" + WildFlyUtil.getHostPort() +
+		// "/catalog/resources/catalog";
+		String serviceName = "dataIngester";
+
+		int port = 8080;
 
 		CuratorFramework curatorFramework = CuratorFrameworkFactory.newClient("localhost:2181",
 				new RetryNTimes(5, 1000));
 		curatorFramework.start();
 		try {
-			
-			ServiceInstance serviceInstance = ServiceInstance.builder().uriSpec(new UriSpec(endpointURI)).address("localhost").port(port).name(serviceName).build();
-			ServiceDiscoveryBuilder.builder(Void.class).basePath("load-balancing-example-dataIngester").client(curatorFramework)
-			.thisInstance(serviceInstance).build().start();
-			
-			
+
+			ServiceInstance serviceInstance = ServiceInstance.builder().uriSpec(new UriSpec(endpointURI))
+					.address("localhost").port(port).name(serviceName).build();
+			ServiceDiscoveryBuilder.builder(Void.class).basePath("load-balancing-example")
+					.client(curatorFramework).thisInstance(serviceInstance).build().start();
+
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
 		try {
-			
+
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	    
-	    //registry.unregisterService(serviceName, endpointURI);
-	    
-	    //registry.discoverServiceURI(serviceName);
-		return "done";
 	}
 
+	@GET
+	@Path("/init")
+	public String initZookeeper() {
+
+		System.out.println("Registering Data Ingester Service");
+		String endpointURI = "localhost:8080/SG_MICROSERVICE_DATAINGESTOR/webapi/dataingester/get";
+		// private final String endpointURI = "http://" + serverName + ":" +
+		// serverPort + "/catalog/resources/catalog";
+		// private final String endpointURI = "http://" +
+		// WildFlyUtil.getHostName() + ":" + WildFlyUtil.getHostPort() +
+		// "/catalog/resources/catalog";
+		String serviceName = "dataIngester";
+
+		int port = 8080;
+
+		CuratorFramework curatorFramework = CuratorFrameworkFactory.newClient("localhost:2181",
+				new RetryNTimes(5, 1000));
+		curatorFramework.start();
+		try {
+
+			ServiceInstance serviceInstance = ServiceInstance.builder().uriSpec(new UriSpec(endpointURI))
+					.address("localhost").port(port).name(serviceName).build();
+			ServiceDiscoveryBuilder.builder(Void.class).basePath("load-balancing-example-dataIngester")
+					.client(curatorFramework).thisInstance(serviceInstance).build().start();
+			
+			return "done registering DataingesterInit";
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return "Not-registered-inDataIngester"; 
+	}
 }
