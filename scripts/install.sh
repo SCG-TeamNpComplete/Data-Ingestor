a40e3a5e0bedbc2891b829225ae89afc79e84ecd
Kedar <kedar.gn20@gmail.com>
2016-11-19 17:09:51 -0500

    changed port #3

--- scripts/install.sh
+++ scripts/install.sh
@@ -16,7 +16,7 @@ do
 		sudo docker ps -a | grep "dataingestor$current_instance" | awk '{print $1}' | xargs --no-run-if-empty sudo docker rm
 	fi
 	#echo "$port:$port"
-	sudo docker run -d -p "$port:$port" --name "dataingestor$current_instance" $(sudo docker images | grep kedargn/dataingestor | awk '{print $3}')
+	sudo docker run -d -p "$port:8080" --name "dataingestor$current_instance" $(sudo docker images | grep kedargn/dataingestor | awk '{print $3}')
 	echo "dataingestor$current_instance instance"
 	current_instance=$((current_instance+1))
 	port=$((port+1))
