63f4a47b8607f7d1d1bd99cf464066ea52883299
asampath <asampath@indiana.edu>
2016-11-21 20:04:46 -0500

    addded curator reference ip

--- SG_MICROSERVICE_DATAINGESTOR/src/main/java/com/scientificgateway/milestone1/DataIngesterManager.java
+++ SG_MICROSERVICE_DATAINGESTOR/src/main/java/com/scientificgateway/milestone1/DataIngesterManager.java
@@ -1,9 +1,12 @@
 package com.scientificgateway.milestone1;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.InetAddress;
 import java.net.URI;
 import java.net.URL;
+import java.util.LinkedList;
 import java.util.List;
 
 import javax.ws.rs.GET;
@@ -12,8 +15,6 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.core.Response;
-
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.retry.RetryNTimes;
@@ -30,6 +31,39 @@ import com.scientificgateway.servicelayer.DataIngesterService;
 
 @Path("/DataIngesterManager")
 public class DataIngesterManager {
+	private static String availableIpAddress = null;
+
+	public DataIngesterManager() {
+
+		List<String> ipaddresses = new LinkedList<>();
+		ipaddresses.add("ec2-35-161-48-143.us-west-2.compute.amazonaws.com");
+		ipaddresses.add("ec2-35-160-137-157.us-west-2.compute.amazonaws.com");
+		ipaddresses.add("ec2-52-15-90-97.us-east-2.compute.amazonaws.com");
+
+		URI uri = null;
+		String response = null;
+		int count = 0;
+		for (String ip : ipaddresses) {
+			count++;
+			try {
+				if (InetAddress.getByName(ip).isReachable(3000)) {
+					availableIpAddress = ip;
+					break;
+				}
+			} catch (IOException e) {
+				System.out.println(e.toString());
+			}
+		}
+		if (count == ipaddresses.size() && availableIpAddress == null) {
+			try {
+				throw new Exception();
+			} catch (Exception e) {
+				System.out.println(e.toString());
+			}
+		}
+
+		// TODO Auto-generated constructor stub
+	}
 
 	private DataIngesterService dataingesterservice;
 
@@ -46,14 +80,22 @@ public class DataIngesterManager {
 		String stationMain = station.replaceAll("\\s+", "");
 
 		System.out.println("in delegate manager-dataingester");
-		/*CuratorFramework curatorFramework = CuratorFrameworkFactory.newClient("localhost:2181",
-				new RetryNTimes(5, 1000));*/
-		CuratorFramework curatorFramework = CuratorFrameworkFactory.newClient("ec2-35-161-48-143.us-west-2.compute.amazonaws.com:2181", new RetryNTimes(5, 1000));
-		  curatorFramework.start();
-		
+		/*
+		 * CuratorFramework curatorFramework =
+		 * CuratorFrameworkFactory.newClient("localhost:2181", new
+		 * RetryNTimes(5, 1000));
+		 */
+		/*
+		 * CuratorFramework curatorFramework =
+		 * CuratorFrameworkFactory.newClient(
+		 * "ec2-35-161-48-143.us-west-2.compute.amazonaws.com:2181", new
+		 * RetryNTimes(5, 1000)); curatorFramework.start();
+		 */
+		CuratorFramework curatorFramework = CuratorFrameworkFactory.newClient(availableIpAddress + ":2181",
+				new RetryNTimes(5, 1000));
 		curatorFramework.start();
 
-		System.out.println("in delegate manager-dataingester");
+		System.out.println("curator start----in delegate manager-dataingester");
 
 		ServiceDiscovery<Void> serviceDiscovery = ServiceDiscoveryBuilder.builder(Void.class)
 				.basePath("load-balancing-example").client(curatorFramework).build();
@@ -96,16 +138,16 @@ public class DataIngesterManager {
 			System.out.println(url);
 
 			System.out.println("hello");
-			
+
 			URIBuilder builder = new URIBuilder();
-			builder.setPath(url).setParameter("station", stationMain).setParameter("date", date).setParameter("hours", hours)
-					.setParameter("minutes", minutes).setParameter("seconds", seconds);
+			builder.setPath(url).setParameter("station", stationMain).setParameter("date", date)
+					.setParameter("hours", hours).setParameter("minutes", minutes).setParameter("seconds", seconds);
 			URI uri = builder.build();
 			System.out.println(uri);
 			HttpGet httpget = new HttpGet(uri);
 			ClientConfig clientConfig = new ClientConfig();
 			Client client = ClientBuilder.newClient(clientConfig);
-			String response =client.target(uri).request().get(String.class);
+			String response = client.target(uri).request().get(String.class);
 			System.out.println(response);
 			return response;
 
